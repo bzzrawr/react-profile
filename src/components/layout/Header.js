589aad528adc6f7be596d9440d6d9ee344643adf
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2021-03-29 21:11:49 +0800

    finalize header part

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container'
-// import PFP from './assets/img/pfp.jpg'
-// import Figure from 'react-bootstrap/Figure';
-// import Carousel from 'react-bootstrap/Carousel';
 
 const Header = () => {
 
     return (
       <div className="mainHeader">
-        <Container>
-          <Row>
-            <Col xs={12} lg={8}>
-              <div className="site-banner caption">
-                <container><h1>Welcome To My Website!</h1>
-                <h2>HEY, I'M SYAFIQ ALI.</h2>
-                <h3>I am a self-learned programmer seeking an opportunity to extend my web and software development skills and knowledge.</h3>
-                <a className="btn btn-xl btn-primary js-scroll-trigger" href="/">WORK IN PROGRESS!</a></container>
-              </div>
-            </Col>
-            <Col xs={6} lg={4}>
-            </Col>
-          </Row>
-        </Container>
+        <div className="site-banner caption">
+          <h1>Welcome To My Website!</h1>
+          <div className="pfp"></div>
+          <h2>HEY, I'M SYAFIQ ALI.</h2>
+          <h3>I am a self-learned programmer seeking an opportunity to extend<br></br> my web and software development skills and knowledge.</h3>
+          <a className="btn btn-xl btn-primary js-scroll-trigger" href="/">CONTACT ME!</a>
+        </div>
       </div>
     )
 }

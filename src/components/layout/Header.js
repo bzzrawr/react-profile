dca32ce5dc8ffb8479ae4b9aecb842483aae8f15
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2021-03-29 18:18:24 +0800

    add animation on navbar and header text

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container'
-import PFP from './assets/img/profile-img.jpg'
-import Figure from 'react-bootstrap/Figure';
+// import PFP from './assets/img/pfp.jpg'
+// import Figure from 'react-bootstrap/Figure';
 // import Carousel from 'react-bootstrap/Carousel';
 
 const Header = () => {
 
     return (
       <div className="mainHeader">
-        <div className="site-banner">
-          <div className="site-banner caption">
-            <h1>Welcome To My Website!</h1>
-            <h2>HEY, I'M SYAFIQ ALI</h2>
-            <h3>This website tells you more on Bob's background.</h3>
-          
-          </div> 
-          <div className="site-banner my-img">
-          </div>
-        
-  
-        </div>
+        <Container>
+          <Row>
+            <Col xs={12} lg={8}>
+              <div className="site-banner caption">
+                <container><h1>Welcome To My Website!</h1>
+                <h2>HEY, I'M SYAFIQ ALI.</h2>
+                <h3>I am a self-learned programmer seeking an opportunity to extend my web and software development skills and knowledge.</h3>
+                <a className="btn btn-xl btn-primary js-scroll-trigger" href="/">WORK IN PROGRESS!</a></container>
+              </div>
+            </Col>
+            <Col xs={6} lg={4}>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
 }
